a76dc644f473a35d2ff4fdd68b597d535bb36052
Anne <annilinum@mail.ru>
2023-01-24 11:29:23 +0300

    css js

--- src/main/java/com/github/annilinum/javaquestionnaire/controller/CategoryController.java
+++ src/main/java/com/github/annilinum/javaquestionnaire/controller/CategoryController.java
@@ -1,9 +1,13 @@
 package com.github.annilinum.javaquestionnaire.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.annilinum.javaquestionnaire.model.Category;
 import com.github.annilinum.javaquestionnaire.service.CategoryService;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,14 +18,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class CategoryController {
   private CategoryService categoryService;
 
-  @GetMapping("/")
+  @SneakyThrows @GetMapping("/")
   public String getCategoriesList(Model model) {
     List<Category> allCategories = categoryService.findAll();
     model.addAttribute("categories_list", allCategories);
-    return "categories-box.html";
+
+/*    var list = List.of("1", "wer");
+    var d = new HashMap<String, String>();
+    for(String item: list) {
+      d.put(book.name, book.ImageUrl);
+    }*/
+
+
+    /*var list = List.of("1", "wer");
+    var d = "{";
+    for(String item: list) {
+      d = d + "{" + item + ": null}";
+    }
+    d = d + "}";*/
+
+    var mm = Map.of(
+        "Apple", "https://i1.sndcdn.com/artworks-000412607283-nrgtef-t240x240.jpg",
+        "Google", "https://i1.sndcdn.com/artworks-000412607283-nrgtef-t240x240.jpg",
+        "Alphine", "https://i1.sndcdn.com/artworks-000412607283-nrgtef-t240x240.jpg",
+        "Smont", "https://i1.sndcdn.com/artworks-000412607283-nrgtef-t240x240.jpg",
+        "Galia", "https://i1.sndcdn.com/artworks-000412607283-nrgtef-t240x240.jpg"
+    );
+    String json = new ObjectMapper().writeValueAsString(mm);
+    model.addAttribute("categories_data", json);
+
+    return "categories-list.html";
   }
+
   @GetMapping("/category/{category_id}/topics")
-  public String getCategoryTopics(@PathVariable("category_id") long category_id, Model model){
+  public String getCategoryTopics(@PathVariable("category_id") long category_id, Model model) {
     model.addAttribute("category_id", category_id);
     model.addAttribute("topics_list", categoryService.getTopicsListByCategoryId(category_id));
     return "topics-list.html";
